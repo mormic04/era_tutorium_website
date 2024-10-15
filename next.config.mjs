28837c1782a9a59e8265f0fd484b0fc4888e562b
mormic04 <m.morandell@tum.de>
2024-10-15 19:18:06 +0200

    set basepath only in production build

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export', // Add this line to enable static export
-  basePath: '/~momi/era', // Set your base path here
+  //basePath: '/~momi/era', // Set your base path here
+  basePath: process.env.NODE_ENV === 'production' ? '/~momi/era' : '',
 };
 
 export default nextConfig;
