6731d39c05e8951817b85c489a720fa712beb7f8
mormic04 <m.morandell@tum.de>
2024-12-05 17:26:57 +0100

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeProvider } from 'next-themes'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <div>
+    {/* Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-K5G5RSBLR8`}
+      />
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K5G5RSBLR8', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
+      {/* ThemeProvider and main app */}
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </div>
   )
 }
