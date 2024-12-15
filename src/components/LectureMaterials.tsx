d3cf63eb8e32d43a445be8af755909242b02fc56
mormic04 <m.morandell@tum.de>
2024-12-15 21:43:48 +0100

    added w09

--- src/components/LectureMaterials.tsx
+++ src/components/LectureMaterials.tsx
@@ -134,6 +134,19 @@ const lectureWeeks = [
       { name: "Tutorslides", url: materialBasePath + "w08/era_tutorslides_w08.pdf"},
       { name: "Writeup zur BNE-Aufgabe (Presentation)", url: materialBasePath + "w08/BNE_Erweiterung_presentation"},
       { name: "Writeup zur BNE-Aufgabe (Text)", url: materialBasePath + "w08/BNE_Erweiterung_writeup"},
+      { name: "Mitschrift 09.12.2024", url: materialBasePath + "w08/era_tutor08_platz_Mo"},
+      { name: "Mitschrift 12.12.2024", url: materialBasePath + "w08/era_tutor08_platz_Do"}
+    ],
+  },
+  {
+    week: 9,
+    date: "09.12.2024",
+    topic: "RISC-V Single-Cycle Prozessor",
+    materials: [
+      { name: "Übungsblatt (leer)", url: "https://www.moodle.tum.de/mod/resource/view.php?id=3249481"},
+      { name: "RISC-V Tabellen und Prozessor-Schaltbilder", url: "https://courses.edx.org/assets/courseware/v1/f06a2dc0c856f60ec0711e9f5e1c98cf/asset-v1:HarveyMuddX+ENGR85B+1T2023+type@asset+block/FinalReferences.pdf"},
+      { name: "Tutorslides", url: materialBasePath + "w09/era_tutorslides_w09.pdf"},
+      { name: "[Template] Aufabe 1d PatternRecognizer.dig", url: materialBasePath + "w09/PatternRecognizer_Template.dig"}
     ],
   },
   
@@ -151,7 +164,7 @@ export default function LectureMaterials() {
     return Math.ceil(diffDays / 7)
   }, [])
 
-  currentWeek = 8;
+  currentWeek = 9;
 
   React.useEffect(() => {
     document.documentElement.classList.toggle('dark', theme === 'dark');
